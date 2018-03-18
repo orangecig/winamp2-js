47db65961e3fb21125bf66efb4693e31f6312f3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-18 05:43:32 +0000

    Deploy captbaritone/winamp2-js to github.com/captbaritone/winamp2-js.git:gh-pages

---
+++ precache-manifest.84d9c5927e53572c156f6cbc75f44d5b.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = [
+  {
+    "url": "/skins/MacOSXAqua1-5-88dbd4e043795c98625462a908a2d965.wsz"
+  },
+  {
+    "url": "/jsmediatags-e074df2f870551a51835.js"
+  },
+  {
+    "url": "/mp3/llama-2.91-deed9f60e1c2a04e753d90c72d57bea1.mp3"
+  },
+  {
+    "url": "/skins/Green-Dimension-V2-4308a2fc648033bf5fe7c4d56a5c8823.wsz"
+  },
+  {
+    "url": "/skins/ZaxonRemake1-0-c7333f1f8fd296f4dd68f9b138d22e5d.wsz"
+  },
+  {
+    "url": "/skins/XMMS-Turquoise-46ab165b574d97e2e51b22cb47a7661c.wsz"
+  },
+  {
+    "url": "/skins/Vizor1-01-0985438a88963f6803311ed99b17f7f4.wsz"
+  },
+  {
+    "url": "/skins/TopazAmp1-2-11f6cf0624da77b1bbe1d48aaef470c2.wsz"
+  },
+  {
+    "url": "/winamp-e074df2f870551a51835.js"
+  },
+  {
+    "url": "/skins/base-2.91-2e12a0351ca76c7567b247edf2cfe0f5.wsz"
+  },
+  {
+    "url": "/images/preview-7f2acefe50050a440ec9d55657c4dda0.png"
+  },
+  {
+    "url": "/images/manifest/icon_192x192.ec9b715176586bf58ec5abe0354efd85.png"
+  },
+  {
+    "url": "/images/manifest/icon_512x512.5228b937afc3368434b594aa65be2c08.png"
+  },
+  {
+    "url": "/images/manifest/icon_513x513.1f27cb4bba614c00bf595696efa4435d.svg"
+  },
+  {
+    "revision": "aa6a5db90de62c8b5e2b9f6909704c51",
+    "url": "/index.html"
+  },
+  {
+    "url": "/images/manifest/icon-96x96-44fece775401fd6184d71022778b4edd.png"
+  },
+  {
+    "url": "/images/manifest/icon-72x72-9d9aaf3f4dad41359616e32e082dc71c.png"
+  },
+  {
+    "url": "/images/manifest/icon-48x48-67470808290acc080f2d154fd1df70d1.png"
+  },
+  {
+    "url": "/images/manifest/icon-192x192-fd139d3437e0cad44d05f85b226a24d7.png"
+  },
+  {
+    "url": "/images/manifest/icon-144x144-06bb87f22f8080f239f096e856435678.png"
+  },
+  {
+    "url": "/images/favicon-0cc27932e7d5cbda2849b27e6e6dc3c8.ico"
+  },
+  {
+    "url": "/css/page-9b2e6182dd916925ea6b1b7cb92d4b0e.css"
+  }
+];
